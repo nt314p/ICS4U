e29c8ede06f5bb32f539c2890a7afe48dcd9aa38
Nick Tong <nicktong.001@gmail.com>
2019-11-24 14:37:23 -0500

    Dialogue delays so you can read what players say

--- Javascript/Go Fish/script.js
+++ Javascript/Go Fish/script.js
@@ -1,11 +1,11 @@
-/* jshint esversion: 6 */
+/* jshint esversion: 8 */
 
 var nameMap = ["shark", "starfish", "whale", "turtle", "octopus", "jellyfish", "crab", "pufferfish", "tuna", "squid", "eel", "seahorse"];
 var colorMap = ["#a5bac9", "#ffde91", "#306cba", "#9aba63", "#9c69cf", "#ed9adb", "#e66060", "#f7e883", "#babbd4", "#ff9cc2", "#ccba91", "#ffdf61"];
-var names = ["Ethan", "Chris", "Christian", "Sonya", "Kaitlin", "Shree"];
-var playerColors = ["red", "orange", "yellow", "green", "blue", "indigo", "purple", "pink"];
+var names = ["Ethan", "Chris", "Christian", "Sonya", "Kaitlin", "Shree", "Karol", "Alex"];
+var playerColors = ["red", "orange", "#fcf11b", "green", "blue", "indigo", "purple", "pink"];
 var initalCards = 5;
-var numPlayers = 8;
+var numPlayers = 6;
 var winningPoints = 10;
 var players = [];
 var canvas, ctx;
@@ -17,9 +17,47 @@ var playerHandR = 330;
 var cardH = 112;
 var cardW = 80;
 var handW = 150;
+var msgX = 400;
+var msgY = 340;
+var msgW = 200;
+var msgH = 120;
 var selectedCard = null;
 var selectedPlayer = null;
+var hoverPlayer = null;
 var clientRect;
+
+class Button {
+    constructor(x, y, w, h, text, color, func) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.text = text;
+        this.color = color;
+        this.func = func;
+        this.down = false;
+    }
+
+    draw() {
+        var x = this.x;
+        var y = this.y;
+        var w = this.w;
+        var h = this.h;
+        ctx.fillStyle = this.color;
+        ctx.fillRect(x - w / 2, y - h / 2, w, h);
+        ctx.fillStyle = "white";
+        ctx.font = "25px Arial";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillText(this.text, x, y);
+        if (mouseX <= x + w / 2 && mouseX >= x - w / 2 && mouseY >= y - h / 2 && mouseY <= y + h / 2) {
+            ctx.strokeStyle = "white";
+            ctx.beginPath();
+            ctx.rect(x - w / 2, y - h / 2, w, h);
+            ctx.stroke();
+        }
+    }
+}
 
 class Player {
     constructor(name, color) {
@@ -28,6 +66,9 @@ class Player {
         this.name = name;
         this.color = color;
         this.selected = false;
+        this.message = null;
+        this.cX = 0;
+        this.cy = 0;
     }
 
     drawCard() {
@@ -71,14 +112,14 @@ class Player {
         }
     }
 
-    takeTurn(player, card) {
-        console.log(this.name + ": Hey " + player.name + ", do you have a " + card.getName() + "?");
+    async takeTurn(player, card) {
+        await this.say("Hey " + player.name + ", do you have a " + card.getName() + "?", 2000);
         if (player.hasCard(card)) { // get card
-            console.log(player.name + ": Yes I do...");
+            await player.say("Yes I do...", 2000);
             this.addCard(player.removeCard(card));
             player.ensureEnoughCards();
         } else { // Go Fish!
-            console.log(player.name + ": Sorry, go fish!");
+            await player.say("Sorry, go fish!", 2000);
             this.drawCard();
         }
         this.removeMatches();
@@ -87,32 +128,84 @@ class Player {
         return this.points >= winningPoints;
     }
 
+    async say(message, ms) {
+        this.message = message;
+        let d = await delay(ms);
+        this.message = null;
+    }
+
     draw(i) {
+        if (this.message != null) {
+            this.drawMsg(this.message);
+        }
+
         var dAng = 360 / players.length;
         var deg = (i * dAng + 90) * Math.PI / 180;
         var hidden = (i != 0);
-        ctx.fillStyle = this.color;
-        var cX = playerIconR * Math.cos(deg) + 400;
-        var cY = playerIconR * Math.sin(deg) + 400;
-        var mouseOver = (900 >= Math.pow(cX - mouseX, 2) + Math.pow(cY - mouseY, 2));
-
-        ctx.beginPath();
-        ctx.ellipse(cX, cY, 30, 30, 0, 0, 2 * Math.PI);
-        ctx.fill();
+        this.cX = playerIconR * Math.cos(deg) + canvas.width / 2;
+        this.cY = playerIconR * Math.sin(deg) + canvas.width / 2;
+        var mouseOver = (900 >= Math.pow(this.cX - mouseX, 2) + Math.pow(this.cY - mouseY, 2));
 
         if (mouseOver && mouseDown) {
-            selectedPlayer = this;
+            if (selectedPlayer == this) {
+                selectedPlayer = null;
+            } else {
+                selectedPlayer = this;
+            }
         }
         this.selected = (selectedPlayer == this);
 
-        if (hidden && (this.selected || mouseOver)) {
-            ctx.strokeStyle = "white";
-            ctx.beginPath();
-            ctx.ellipse(cX, cY, 30, 30, 0, 0, 2 * Math.PI);
-            ctx.stroke();
+        if (mouseOver) {
+            hoverPlayer = this;
+        } else if (hoverPlayer == this) {
+            hoverPlayer = null;
         }
 
+        if (hidden && ((this.selected && (hoverPlayer == this || hoverPlayer == null)) || mouseOver)) {
+
+            ctx.strokeStyle = "white";
+            ctx.beginPath(); // player's circle icon
+            ctx.ellipse(this.cX, this.cY, 31, 31, 0, 0, 2 * Math.PI);
+            ctx.stroke();
+
+            // ctx.beginPath(); // line pointing to player
+            // ctx.moveTo(msgX, msgY);
+            // ctx.lineTo(cX, cY);
+            // ctx.stroke();
+            // ctx.fillStyle = "white";
+            // ctx.fillRect(msgX - msgW / 2, msgY - msgH / 2, msgW, msgH); // message box
+            // ctx.strokeStyle = "#5da4f0";
+            // ctx.strokeRect(msgX - msgW / 2, msgY - msgH / 2, msgW, msgH); // outer card stroke
+            // ctx.fillStyle = "#5da4f0";
+            // ctx.font = "30px Arial";
+            // ctx.fillText(this.name, msgX, 300);
+            // ctx.font = "20px Arial";
+            this.drawMsg("Points: " + this.points);
+        }
+
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.ellipse(this.cX, this.cY, 30, 30, 0, 0, 2 * Math.PI);
+        ctx.fill();
+
         this.hand.drawHand(playerHandR * Math.cos(deg) + 400, playerHandR * Math.sin(deg) + 400, deg * 180 / Math.PI - 90, hidden);
+    }
+
+    drawMsg(text) {
+        ctx.strokeStyle = "white";
+        ctx.beginPath(); // line pointing to player
+        ctx.moveTo(msgX, msgY);
+        ctx.lineTo(this.cX, this.cY);
+        ctx.stroke();
+        ctx.fillStyle = "white";
+        ctx.fillRect(msgX - msgW / 2, msgY - msgH / 2, msgW, msgH); // message box
+        ctx.strokeStyle = "#5da4f0";
+        ctx.strokeRect(msgX - msgW / 2, msgY - msgH / 2, msgW, msgH); // outer card stroke
+        ctx.fillStyle = "#5da4f0";
+        ctx.font = "30px Arial";
+        ctx.fillText(this.name, msgX, 300);
+        ctx.font = "20px Arial";
+        wrapText(text, msgX, msgY, 30, msgW - 20);
     }
 }
 
@@ -273,7 +366,11 @@ class Card {
     draw(x, y, deg, flipped, mouseOver) {
 
         if (mouseOver && mouseDown) {
-            selectedCard = this;
+            if (selectedCard == this) {
+                selectedCard = null;
+            } else {
+                selectedCard = this;
+            }
         }
         this.selected = (selectedCard == this);
 
@@ -312,12 +409,12 @@ class Card {
 
             ctx.fillStyle = colorMap[this.cardNum];
             ctx.fillRect(- cardW / 2, - cardH / 2, cardW, cardH); // main rectangle
-            ctx.fillStyle = "black";
+            ctx.fillStyle = "white";
             ctx.font = "15px Arial";
             ctx.textAlign = "center";
-            ctx.fillText(this.getName().capitalize(), 0, 25 - cardH / 2);
+            ctx.fillText(this.getName().capitalize(), 0, 20 - cardH / 2);
             ctx.font = "70px Arial";
-            ctx.fillText(this.cardNum, 0, 100 - cardH / 2);
+            ctx.fillText(this.cardNum, 0, 70 - cardH / 2);
         }
 
         ctx.rotate(-deg * Math.PI / 180); // untranslate
@@ -341,6 +438,10 @@ Array.prototype.popRandomElement = function () {
     return this.splice(Math.floor(Math.random() * this.length), 1)[0];
 };
 
+Array.prototype.randomElement = function () {
+    return this[Math.floor(Math.random() * this.length)];
+};
+
 String.prototype.capitalize = function () {
     return this.charAt(0).toUpperCase() + this.slice(1);
 };
@@ -353,12 +454,50 @@ function randomElement(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
 }
 
-// -- Actual Game Code -- //
+function sleep(ms) {
+    var initial = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - initial) > ms) {
+            break;
+        }
+    }
+}
 
-window.onload = function () {    // initialization
+// from https://www.html5canvastutorials.com/tutorials/html5-canvas-wrap-text-tutorial/
+function wrapText(text, x, y, lineHeight, maxWidth) {
+    var words = text.split(' ');
+    var line = '';
+    for (var n = 0; n < words.length; n++) {
+        var testLine = line + words[n] + ' ';
+        var metrics = ctx.measureText(testLine);
+        var testWidth = metrics.width;
+        if (testWidth > maxWidth && n > 0) {
+            ctx.fillText(line, x, y);
+            line = words[n] + ' '; line = words[n] + ' ';
+            y += lineHeight;
+        }
+        else {
+            line = testLine;
+        }
+    }
+    ctx.fillText(line, x, y);
+}
+
+// from https://stackoverflow.com/questions/17883692
+async function delay(delayInms) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(2);
+        }, delayInms);
+    });
+}
+
+
+window.onload = function () {    // initialization -----
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
     clientRect = canvas.getBoundingClientRect();
+    ctx.textBaseline = "middle";
 
     document.addEventListener('mousemove', e => {
         clientRect = canvas.getBoundingClientRect();
@@ -369,9 +508,9 @@ window.onload = function () {    // initialization
     document.addEventListener('mousedown', e => {
         mouseDown = true;
     });
-    document.addEventListener('mouseup', e => {
-        mouseDown = false;
-    });
+    // document.addEventListener('mouseup', e => {
+    //     mouseDown = false;
+    // });
 
     players[0] = new HumanPlayer("GOD");
     draw();
@@ -379,7 +518,7 @@ window.onload = function () {    // initialization
     for (var i = 0; i < numPlayers; i++) {
         if (i != 0) {
             players[i] = new RandomPlayer();
-            //document.getElementById("r" + i).innerHTML = players[i].name;
+            document.getElementById("r" + i).innerHTML = players[i].name;
         }
         for (var j = 0; j < initalCards; j++) {
             players[i].drawCard();
@@ -389,12 +528,17 @@ window.onload = function () {    // initialization
     console.log(players[0].hand.toString());
 };
 
+var turnButton = new Button(400, 500, 140, 80, "Take Turn", "#3464c9", 1);
+
 function draw() {
     ctx.fillStyle = "lightblue";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     for (var i = 0; i < players.length; i++) {
         players[i].draw(i);
+    }
+    if (selectedCard != null && selectedPlayer != null) {
+        turnButton.draw();
     }
 
     window.requestAnimationFrame(draw);
@@ -413,7 +557,7 @@ function stepTurns() {
     }
 }
 
-function HPT() {
+async function HPT() {
     console.clear();
     var playerNum;
     var cardNum = document.getElementById("cardval").value;
@@ -424,12 +568,14 @@ function HPT() {
         }
     }
 
-    var won = players[0].takeTurn(players[playerNum], new Card(cardNum));
+    var won = await players[0].takeTurn(players[playerNum], new Card(cardNum));
     for (i = 1; i < numPlayers; i++) {
-        if (won || players[i].takeTurn()) {
+        await players[i].takeTurn();
+        if (won) { // fix player winning
             console.log(players[i].name + " wins!");
             break;
         }
+        //let d = await delay(1000);
     }
     console.log(players[0].hand.toString());
 }
