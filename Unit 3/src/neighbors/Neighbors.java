0d5449dd3bde67f77fe53f340b3555c3d1b00f93
Nick Tong <32203145+nt314p@users.noreply.github.com>
2020-01-11 15:03:20 -0500

    More work on neighbors

--- Unit 3/src/neighbors/Neighbors.java
+++ Unit 3/src/neighbors/Neighbors.java
@@ -2,38 +2,40 @@ package neighbors;
 
 public class Neighbors {
 	static int[] donations;
-	static int[] collected;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		donations = new int[] {4, 8, 5, 8, 4, 3};
+		//
+		donations = new int[] { 8, 7, 6, 5, 4, 3, 2, 1, 9 };
 		System.out.println(solve());
 	}
 
 	public static int solve() {
-		int maxDonations = 0;
+
+	}
+
+	public static int[] solveList() {
 		if (donations.length == 1) // quick fix
-			return donations[0];
-		collected = new int[donations.length];
-		for (int i = 0; i < collected.length; ++i) // -1: undecided, 0: no collection, 1: collect
-			collected[i] = -1; // initialize array with undecided
-		for (int i = 0; i < collected.length; ++i) { // iterate through donations
-			boolean isWorth = (value(i) >= value(i - 1) + value(i + 1));
+			return new int[] { 1 };
+		int[] collect = new int[donations.length];
+		for (int i = 0; i < collect.length; ++i) // -1: undecided, 0: no collection, 1: collect
+			collect[i] = -1; // initialize array with undecided
+		for (int i = 0; i < collect.length; ++i) { // iterate through donations
+			boolean isWorth = (value(i, collect) >= value(i - 1, collect) + value(i + 1, collect));
 			if (isWorth) { // is the current index worth more than its neighbors
-				collected[posIndex(i - 1)] = 0; // front and back are not to be collected
-				collected[posIndex(i + 1)] = 0;
+				collect[posIndex(i - 1)] = 0; // front and back are not to be collected
+				collect[posIndex(i + 1)] = 0;
 			}
-			collected[posIndex(i)] = isWorth ? 1 : 0; // isWorth decides whether donation is collected
-			maxDonations += donations[i] * collected[i]; // collected donation
+			collect[posIndex(i)] = isWorth ? 1 : 0; // isWorth decides whether donation is collected
 		}
-		return maxDonations;
+		return collect;
 	}
 
 	public static int posIndex(int index) { // reassign index to positive value
-		return Math.floorMod(index, collected.length);
+		return Math.floorMod(index, donations.length);
 	}
 
-	public static int value(int i) { // return 0 if no donations being collected
+	public static int value(int i, int[] collected) { // return 0 if no donations being collected
 		return Math.abs(collected[posIndex(i)] * donations[posIndex(i)]);
 	}
 
